aeebe26be8f3760197e4f22e5fe0a6b69c0037e2
MJGudgin <MJGudgin@live.com>
2020-07-02 22:58:51 -0700

    Added bulb/flower/blossoms and fixed script to copy to new directory

--- script.ps1
+++ script.ps1
@@ -1,2 +1,2 @@
-Copy-Item *.filter 'D:\Documents\my games\Path of Exile\'
-Copy-Item *.wav 'D:\Documents\my games\Path of Exile\'
+Copy-Item *.filter 'Z:\Documents\my games\Path of Exile\'
+Copy-Item *.wav 'Z:\Documents\my games\Path of Exile\'
