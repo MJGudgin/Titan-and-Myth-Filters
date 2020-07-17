f93dbde6884211c84de67f2fca7740094d885825
MJGudgin <MJGudgin@live.com>
2020-07-17 14:44:51 -0700

    Changed Public filter name and added PS line to copy private filter without the custom sound calls

--- script.ps1
+++ script.ps1
@@ -1,2 +1,3 @@
+Select-String -Path SSF.filter -Pattern 'CustomAlertSound' -NotMatch -Raw | Out-File Public.filter
 Copy-Item *.filter 'Z:\Documents\my games\Path of Exile\'
 Copy-Item *.wav 'Z:\Documents\my games\Path of Exile\'
