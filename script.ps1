d47e0d24e6a625b370981968603f7533b74cd625
MJGudgin <MJGudgin@live.com>
2021-04-15 11:33:19 -0700

    Removed the new powershell script as the original script was already performing the same function

--- script.ps1
+++ script.ps1
@@ -1,3 +1,3 @@
-Select-String -Path SSF.filter -Pattern 'CustomAlertSound' -NotMatch -Raw | Out-File Public.filter
+Select-String -Path SSF.filter -Pattern 'CustomAlertSound' -NotMatch -Raw | Out-File Console.filter
 Copy-Item *.filter 'Z:\Documents\my games\Path of Exile\'
 Copy-Item *.wav 'Z:\Documents\my games\Path of Exile\'
