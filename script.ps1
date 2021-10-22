65f1c30a19a4998c5a0204ccc26b6a79d5061f68
Matthew <mjudgin@live.com>
2021-10-22 10:59:15 -0700

    Initial Scourge Support

--- script.ps1
+++ script.ps1
@@ -1,3 +1,3 @@
 Select-String -Path SSF.filter -Pattern 'CustomAlertSound' -NotMatch -Raw | Out-File Console.filter
-Copy-Item *.filter 'Z:\Documents\my games\Path of Exile\'
-Copy-Item *.wav 'Z:\Documents\my games\Path of Exile\'
+Copy-Item *.filter 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile\'
+Copy-Item *.wav 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile\'
