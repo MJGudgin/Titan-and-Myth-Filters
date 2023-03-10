0f7643509d5d3300bd112aaf9e41e4bfb3293488
Matthew <mjudgin@live.com>
2023-03-09 19:36:38 -0700

    Updated for Sanctum and included functionality for flagging sections of the filter to be ignored for the Strict version of the filter

--- script.ps1
+++ script.ps1
@@ -1,3 +1,35 @@
-Select-String -Path SSF.filter -Pattern 'CustomAlertSound' -NotMatch -Raw | Out-File Console.filter
+#Select-String -Path SSF.filter -Pattern 'CustomAlertSound' -NotMatch -Raw | Out-File Console.filter
+
+$file = "SSF.filter"
+rm Console.filter
+New-Item Console.filter
+
+rm SSF-Strict.filter
+New-Item SSF-Strict.filter
+
+$objCount
+$StrictOutput = $true
+
+foreach ($line in (Get-Content $file))
+{
+		$a = $line.Split(' ')
+		if ($a[0] -ne '	CustomAlertSound')
+		{
+			Add-Content Console.filter $line
+		}
+		if ($a[0] -eq '#StrictIgnore')
+		{
+			$StrictOutput = $false
+		}
+		if ($a[0] -eq '#EndStrictIgnore')
+		{
+			$StrictOutput = $true
+		}
+		elseif ($StrictOutput -eq $true)
+		{
+			Add-Content SSF-Strict.filter $line
+		}
+}
+
 Copy-Item *.filter 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile\'
 Copy-Item *.wav 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile\'
